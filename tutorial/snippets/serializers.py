ebbb4372b96a56d150021d55d941679c8556a661
RakeshKumar <92.rakesh@gmail.com>
2018-07-20 21:05:21 +0530

    end of part 4

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,26 +1,9 @@
-from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelated(many=True, query=Snippet.objects.all())
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
-        model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
-
-    def create(self, validated_date):
-        """
-        create and return a new 'Snippet' instance, given the validated
-        """
-        return Snippet.objects.create(**validated_data)
-        
-    def update(self, instance, validated_data):
-        """
-        update and return an existing 'Snippet' instance, given the validated data.
-        """
-        instance.title = validated_data.get('title',instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos',instance.linenos)
-        instance.language = validated_data.get('language',instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
-
+        model = User
+        fields = ('id', 'username', 'snippets','owner')
